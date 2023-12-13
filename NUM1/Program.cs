c2c7af21be61da3cd8aec509b4f4e7eaa63a61c8
Maria Andreeva <maria.andreeva.marketing@gmail.com>
2023-12-13 21:56:07 +0100

    Added DZ number 1

---
+++ NUM1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. 
+// Использовать рекурсию, не использовать циклы.
+
+void ShowNaturNumber(int m, int n)
+{
+    if(n <= 0 || m <= 0)
+    {
+        Console.WriteLine("Numb must be graiter then 0");
+        return;
+    }
+
+    Console.Write(m++ + " ");
+
+    if (m <= n)
+    {
+        ShowNaturNumber(m, n);
+    }
+}
+
+ShowNaturNumber(1, 10);
+
