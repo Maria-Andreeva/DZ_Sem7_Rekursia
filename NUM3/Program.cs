86486d4fe12c3bba86ce4168f347cec396e610e0
Maria Andreeva <maria.andreeva.marketing@gmail.com>
2023-12-14 00:22:14 +0100

    Added DZ number 3

---
+++ NUM3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. 
+// Использовать рекурсию, не использовать циклы.
+
+Console.Clear();
+
+int[] PrintArray (int[] myArray)
+{
+    if(myArray.Length > 0)
+    {
+        Console.Write(myArray[myArray.Length - 1] + " ");
+
+        var temparray = myArray.ToList();
+        temparray.RemoveAt(myArray.Length - 1);
+
+        return PrintArray(temparray.ToArray());
+    }
+
+    return null!;
+    
+}
+
+int[] myArray = {1, 2, 3, 4, 5};
+PrintArray(myArray);
+
+
